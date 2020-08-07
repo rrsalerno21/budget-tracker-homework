2f96f073d0d382316e67c45b993a464fda22b397
Rocky <rrsalerno21@gmail.com>
2020-08-07 12:42:36 -0700

    setup mLab

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
